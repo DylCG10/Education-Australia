ac81880df5b3c77b20004981c7a496547f56b30c
Dylan Gonzalez <dylan.claudio.g@gmail.com>
2021-02-02 13:51:58 +1100

    2nd possible design
    Just testing stuff out

---
+++ Test2/js/main.js
@@ -0,0 +1,12 @@
+//Option 2 - jQuery Smooth Scroll --> cross platform solution
+$('.navbar a').on('click', function(e) {
+    if(this.hash !== '') {
+        e.preventDefault();
+
+        const hash = this.hash;
+
+        $('html, body').animate({
+            scrollTop: $(hash).offset().top
+        }, 800);
+    }
+})
